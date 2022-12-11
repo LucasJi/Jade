0efa55b875e2257eb19dcc6b5ed07f3e0d65121c
lucas <ymfs.l.j@outlook.com>
2022-12-11 21:52:38 +0800

    create a Navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>{`Lucas Ji's Blog`}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        Welcome to my blog!(WIP...) 
-      </main>
+      <main>Welcome to my blog!(WIP...)</main>
 
-      <footer className={styles.footer}>Copyright by Lucas Ji</footer>
+      {/* <footer>Copyright by Lucas Ji</footer> */}
     </div>
   );
 }
