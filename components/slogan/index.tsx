dbef8a252b91c5860bf39e2f755113b04ae1fd2c
lucas <ymfs.l.j@outlook.com>
2023-02-21 10:10:34 +0800

    use typewriter library

--- components/slogan/index.tsx
+++ components/slogan/index.tsx
@@ -1,10 +1,30 @@
 import classNames from 'classnames';
+import Typewriter from 'typewriter-effect';
 import styles from './slogan.module.scss';
 
 export default function Slogan({ className }: { className?: string }) {
   return (
-    <div className={classNames(styles.slogan, className)}>
+    <div>
       You are more than what you have become!
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString('Hi, I am a full stack developer!')
+            .callFunction(() => {
+              console.log('String typed out!');
+            })
+            .pauseFor(2500)
+            .deleteAll()
+            .callFunction(() => {
+              console.log('All strings were deleted');
+            })
+            .typeString('My name is Lucas Ji.')
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString('Welcome to my digital garden!')
+            .start();
+        }}
+      />
     </div>
   );
 }
