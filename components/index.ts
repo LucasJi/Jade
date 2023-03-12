f03f3c87402be4cb1a3daea433dc3b96a59cd706
lucas <ymfs.l.j@outlook.com>
2023-03-12 13:32:40 +0800

    create a test page to test remark wiki link plugin

--- components/index.ts
+++
@@ -1,7 +0,0 @@
-import Navbar from './navbar';
-import Markdown from './markdown';
-import Avatar from './avatar';
-import Slogan from './slogan';
-import Test from './test';
-
-export { Navbar, Markdown, Avatar, Slogan, Test };
