92e949dc71efad8c512677dd90f67a18189d110a
lucas <ymfs.l.j@outlook.com>
2023-03-12 20:08:09 +0800

    remove some useless dependencies

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,5 @@
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
 import rehypeDocument from 'rehype-document';
 import rehypeFormat from 'rehype-format';
 import rehypeStringify from 'rehype-stringify';
