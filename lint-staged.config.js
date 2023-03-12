40d3d27d56e761129a81599f619ec2b6e2033e2b
lucas <ymfs.l.j@outlook.com>
2023-03-12 22:52:10 +0800

    remove prettier stage from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // This will lint and format TypeScript and                                             //JavaScript files
   '**/*.(ts|tsx|js)': filenames => [
     `pnpm eslint --fix ${filenames.join(' ')}`,
-    `pnpm prettier --write ${filenames.join(' ')}`,
+    // `pnpm prettier --write ${filenames.join(' ')}`,
   ],
 
   // this will Format MarkDown and JSON
