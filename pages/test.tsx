31cc92ba165c35e07655fed4bc7bec8d0cda1ec0
lucas <ymfs.l.j@outlook.com>
2023-03-13 22:58:56 +0800

    test husky again

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,7 @@
-import fs from 'fs';
-
 export default function Test() {
   return (
     <div>
-      <div>sdfdsf</div>test page 1
+      <div>sdfdsf</div>test page 1<div>sdfdsf</div>test page 1
     </div>
   );
 }
