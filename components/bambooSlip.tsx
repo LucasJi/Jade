521a88675c7b7f9d853ffef563a2426ea74489a9
lucas <ymfs.l.j@outlook.com>
2023-06-16 14:57:42 +0800

    complete view viewed post func

--- components/bambooSlip.tsx
+++ components/bambooSlip.tsx
@@ -16,19 +16,23 @@ function BambooSlip({ post }: { post: Post }) {
   // always display the selected one and its next two posts(only if it has)
   const [anchor, setAnchor] = useState<number>(0);
 
+  console.log('posts', posts);
+
   const wikilinkRender = ({
     className,
     href,
     children,
+    currentPostWikilink,
     ...props
   }: {
     className: string | undefined;
     href: string | undefined;
+    currentPostWikilink: string;
     children: ReactNode;
   }) => {
     const isWikiLink = className?.includes('wikilink');
     return isWikiLink && href ? (
-      <Wikilink onClick={() => viewPost(href)} wikilink={href}>
+      <Wikilink onClick={viewPost(currentPostWikilink)} wikilink={href}>
         {children}
       </Wikilink>
     ) : (
@@ -37,9 +41,14 @@ function BambooSlip({ post }: { post: Post }) {
   };
 
   const viewPost = (currentPostWikilink: string) => (toView: Post) => {
+    console.log('current wikilink', currentPostWikilink);
+
     const currentPostIdx = posts.findIndex(
       p => p.wikilink === currentPostWikilink,
     );
+
+    console.log('posts', posts);
+    console.log('current idx', currentPostIdx);
 
     if (isViewed(toView)) {
       viewViewedPost(currentPostIdx, toView);
@@ -54,13 +63,26 @@ function BambooSlip({ post }: { post: Post }) {
   const viewViewedPost = (from: number, toView: Post) => {
     const toViewIdx = posts.findIndex(p => p.wikilink === toView.wikilink);
 
-    if (Math.abs(toViewIdx - from) > 1 && toViewIdx + 2 <= posts.length - 1) {
+    // view next or pre post, maybe self
+    if (Math.abs(toViewIdx - from) <= 1) {
+      // view next
+      if (toViewIdx > from) {
+        if (Math.abs(anchor - from) > 1) {
+          setAnchor(pre => pre + 1);
+        }
+      } else if (toViewIdx < from) {
+        // view pre
+        if (anchor === from) {
+          setAnchor(pre => pre - 1);
+        }
+      }
+    } else {
       setAnchor(toViewIdx);
     }
   };
 
   const viewNewPost = (from: number, toView: Post) => {
-    const adjustedPosts = posts.slice(0, from);
+    const adjustedPosts = posts.slice(0, from + 1);
     adjustedPosts.push(toView);
     setPosts(adjustedPosts);
 
@@ -73,9 +95,9 @@ function BambooSlip({ post }: { post: Post }) {
   const isExpended = (wikilink: string) => {
     let expendedPosts;
     if (anchor + 2 <= posts.length - 1) {
-      expendedPosts = posts.slice(anchor, anchor + 2);
+      expendedPosts = posts.slice(anchor, anchor + 3);
     } else if (anchor + 1 <= posts.length - 1) {
-      expendedPosts = posts.slice(anchor, anchor + 1);
+      expendedPosts = posts.slice(anchor, anchor + 2);
     } else {
       expendedPosts = [posts[anchor]];
     }
@@ -95,7 +117,12 @@ function BambooSlip({ post }: { post: Post }) {
                 p: ({ node, ...props }) => <div {...props} />,
                 a: props => {
                   const { className, href, children } = props;
-                  return wikilinkRender({ className, href, children });
+                  return wikilinkRender({
+                    className,
+                    href,
+                    children,
+                    currentPostWikilink: wikilink,
+                  });
                 },
                 // eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
                 pre: ({ node, ...props }) => (
