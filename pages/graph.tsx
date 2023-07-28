e48723c16c578add02cab38ba7655312e8cd0509
lucas <ymfs.l.j@outlook.com>
2023-07-28 10:22:42 +0800

    save changes

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -3,6 +3,46 @@ import { Nodes } from '@components';
 import useStarryStore from '@store';
 import { useEffect } from 'react';
 import httpClient from '@utils/axios';
+import { Vector3 } from 'three';
+
+const fibVal = [0, 1];
+const fib = (n: number) => {
+  const len = fibVal.length;
+
+  if (n <= len - 1) {
+    return fibVal[n];
+  }
+
+  for (let i = len; i <= n; i++) {
+    fibVal.push(fibVal[i - 1] + fibVal[i - 2]);
+  }
+  return fibVal[n];
+};
+
+const postCount = 12;
+
+for (let i = 1; i <= postCount; i++) {
+  const len = fib(i) * 10;
+  const x = len;
+  const y = 0;
+
+  const circleNumber = i;
+  const baseAngle = Math.PI / 2;
+  const angle = baseAngle / circleNumber;
+
+  const pos = new Vector3(x, y, 0);
+  const origin = new Vector3(0, 0, 0);
+  if (i <= 3 && i > 2) {
+    console.log(pos);
+  } else if (i > 3) {
+    for (let count = 1; count <= circleNumber - 3; count++) {
+      const rotated = pos.clone().applyAxisAngle(origin, Math.PI / 4);
+      const cX = x - len * Math.cos(angle * count);
+      const cY = y + len * Math.sin(angle * count);
+      console.log(rotated);
+    }
+  }
+}
 
 export default function Graph() {
   const { nodeMap, postMap, initPostMap } = useStarryStore();
@@ -12,8 +52,6 @@ export default function Graph() {
       initPostMap(data);
     });
   }, []);
-
-  console.log('postMap:', postMap);
 
   return (
     <Canvas
