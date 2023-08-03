7f1bb631e6617d8b18209f01ed7cc7afbe9e97d0
lucas <ymfs.l.j@outlook.com>
2023-08-04 00:44:12 +0800

    save changes

--- pages/forceDirectedGraph.jsx
+++ pages/forceDirectedGraph.jsx
@@ -1,6 +1,6 @@
 //useSWR allows the use of SWR inside function components
 import useSWR from 'swr';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { scaleOrdinal } from 'd3-scale';
 import { schemeCategory10 } from 'd3-scale-chromatic';
 import {
@@ -9,19 +9,26 @@ import {
   forceManyBody,
   forceSimulation,
 } from 'd3-force';
+import { select } from 'd3-selection';
+import { drag } from 'd3-drag';
 
 const fetcher = url => fetch(url).then(res => res.json());
 export default function ForceDirectedGraph() {
   const { data, error, isLoading } = useSWR('/api/testdata', fetcher);
   const [animatedLinks, setAnimatedLinks] = useState([]);
   const [animatedNodes, setAnimatedNodes] = useState([]);
+  const nodeGRef = useRef();
 
-  // Specify the dimensions of the chart.
-  const width = 928;
-  const height = 600;
-
-  // Specify the color scale.
-  const color = scaleOrdinal(schemeCategory10);
+  const { width, height, color } = useMemo(
+    () => ({
+      // Specify the dimensions of the chart.
+      width: 900,
+      height: 600,
+      // Specify the color scale.
+      color: scaleOrdinal(schemeCategory10),
+    }),
+    [],
+  );
 
   useEffect(() => {
     if (!data) {
@@ -44,8 +51,20 @@ export default function ForceDirectedGraph() {
       setAnimatedLinks([...links]);
     });
 
+    console.log('node g ref', nodeGRef.current);
+    const circles = select(nodeGRef.current).selectAll('circle');
+    console.log('circles', circles);
+
+    circles.call(
+      drag().on('drag', event => {
+        console.log(event);
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }),
+    );
+
     return () => simulation.stop();
-  }, [data]);
+  }, [data, nodeGRef.current]);
 
   if (error) {
     return <div>failed to load</div>;
@@ -84,7 +103,7 @@ export default function ForceDirectedGraph() {
         })}
       </g>
 
-      <g stroke="#fff" strokeWidth={1.5}>
+      <g ref={nodeGRef} stroke="#fff" strokeWidth={1.5}>
         {animatedNodes.map(node => (
           <circle
             cx={node.x}
