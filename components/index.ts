73b7df9f65e4eca9acc24945bbaeb0abd49b093e
lucas <ymfs.l.j@outlook.com>
2023-08-06 20:23:26 +0800

    save changes

--- components/index.ts
+++ components/index.ts
@@ -4,18 +4,5 @@ import Avatar from './avatar';
 import Slogan from './slogan';
 import Wikilink from './wikilink';
 import BambooSlip from './bambooSlip';
-import Node from './node';
-import Nodes from './nodes';
-import Circle from './circle';
 
-export {
-  Navbar,
-  Markdown,
-  Avatar,
-  Slogan,
-  Wikilink,
-  BambooSlip,
-  Node,
-  Circle,
-  Nodes,
-};
+export { Navbar, Markdown, Avatar, Slogan, Wikilink, BambooSlip };
