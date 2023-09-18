4d9bb6cea798c09ae7a41004be3b91ccd2d3c02c
lucas <ymfs.l.j@outlook.com>
2023-09-18 17:12:25 +0800

    style: install menu component

--- store/index.tsx
+++ store/index.tsx
@@ -1,29 +1,9 @@
 import { create, StateCreator } from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { PostMap } from '@types';
-import { Vector3 } from 'three';
 
 const middleware = (f: StateCreator<StoreState>) => devtools(f);
 
-interface StoreState {
-  updatePostPos: (name: string, pos: Vector3) => void;
-  postMap: PostMap;
-  initPostMap: (initPostMap: PostMap) => void;
-}
+interface StoreState {}
 
-const useStarryStore = create<StoreState>()(
-  middleware(set => ({
-    updatePostPos: (name: string, pos: Vector3) =>
-      set(({ postMap }) => {
-        return {
-          postMap: { ...postMap, [name]: { ...postMap[name], position: pos } },
-        };
-      }),
-    postMap: {},
-    initPostMap: (initPostMap: PostMap) =>
-      set(() => {
-        return { postMap: initPostMap };
-      }),
-  })),
-);
+const useStarryStore = create<StoreState>()(middleware(set => ({})));
 export default useStarryStore;
