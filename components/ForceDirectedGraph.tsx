4f96c82a7bc9db0d1ce05eec4d1454f8c04bd948
lucas <ymfs.l.j@outlook.com>
2023-10-07 07:10:18 +0800

    feat: upgrade post graph component

--- components/ForceDirectedGraph.tsx
+++ components/ForceDirectedGraph.tsx
@@ -14,13 +14,14 @@ import {
 import { PostGraph, PostGraphLink, PostGraphNode } from '@types';
 
 const ForceDirectedGraph = ({ postGraph }: { postGraph: PostGraph }) => {
-  const { width, height, color } = useMemo(
+  const { width, height, color, r } = useMemo(
     () => ({
       // Specify the dimensions of the chart.
       width: 900,
       height: 600,
       // Specify the color scale.
       color: scaleOrdinal(schemeCategory10),
+      r: 8,
     }),
     [],
   );
@@ -54,13 +55,28 @@ const ForceDirectedGraph = ({ postGraph }: { postGraph: PostGraph }) => {
       viewBox={`0 0 ${width} ${height}`}
       width={width}
     >
-      <g stroke="#999" strokeOpacity={0.8}>
+      <g stroke="black">
+        <defs>
+          <marker
+            id="arrow"
+            markerHeight="10"
+            markerUnits="strokeWidth"
+            markerWidth="10"
+            orient="auto"
+            refX="20"
+            refY="6"
+            viewBox="0 0 12 12"
+          >
+            <path d="M2,2 L10,6 L2,10 L6,6 L2,2"></path>
+          </marker>
+        </defs>
         {simulationLinks.map(({ source, target, index }) => {
           const sourceNode = source as PostGraphNode;
           const targetNode = target as PostGraphNode;
           return (
             <line
               key={index}
+              markerEnd="url(#arrow)"
               strokeWidth={1}
               x1={sourceNode.x}
               x2={targetNode.x}
@@ -72,15 +88,31 @@ const ForceDirectedGraph = ({ postGraph }: { postGraph: PostGraph }) => {
       </g>
       <g stroke="#fff" strokeWidth={1.5}>
         {simulationNodes.map(node => (
-          <circle
-            cx={node.x}
-            cy={node.y}
-            fill={color(node.slugIdx!.toString())}
+          // <Tooltip content={node.title} key={node.wikilink}>
+          //   <circle
+          //     cx={node.x}
+          //     cy={node.y}
+          //     fill={color(node.slugIdx!.toString())}
+          //     r={8}
+          //   />
+          // </Tooltip>
+          <g
             key={node.wikilink}
-            r={8}
+            transform={` translate(${node.x}, ${node.y}) `}
           >
-            <title>{node.title}</title>
-          </circle>
+            <circle fill={color(node.slugIdx!.toString())} r={r} />
+            <text
+              dx="0"
+              dy="0"
+              style={{
+                fill: 'black',
+                fontSize: '0.6rem',
+                stroke: 0,
+              }}
+            >
+              {node.title}
+            </text>
+          </g>
         ))}
       </g>
     </svg>
