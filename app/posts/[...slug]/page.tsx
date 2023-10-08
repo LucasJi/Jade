1b93386392331221734dfbafa6052a6eb4a901b6
lucas <ymfs.l.j@outlook.com>
2023-10-08 21:20:40 +0800

    feat: refactor post slug page

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,13 +1,46 @@
-import BambooSlip from '@components/bambooSlip';
 import { Slug } from '@types';
 import { getCachedSlugs, getPostBySlug } from '@utils/postUtil';
+import classNames from 'classnames';
+import Markdown from '@components/Markdown';
+import Wikilink from '@components/Wikilink';
 
 export default async function Post({ params }: { params: { slug: Slug } }) {
   const post = getPostBySlug(params.slug);
+
   if (!post) {
     return <div>POST NOT FOUND</div>;
   }
-  return <BambooSlip post={post} />;
+
+  const { wikilink, content, backlinks } = post;
+
+  return (
+    <div
+      className={classNames(
+        'break-words',
+        'flex',
+        'flex-col',
+        'px-4',
+        'overflow-y-auto',
+        'h-full',
+        'pt-4',
+      )}
+      key={`content-${wikilink}`}
+    >
+      <Markdown markdown={content} />
+      <div>
+        <div className="bg-green-200 font-bold">Backlinks</div>
+        {backlinks.length > 0 ? (
+          backlinks.map(bl => (
+            <Wikilink key={bl} wikilink={bl}>
+              {bl}
+            </Wikilink>
+          ))
+        ) : (
+          <div className="bg-yellow-600">No Backlinks</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export async function generateStaticParams() {
