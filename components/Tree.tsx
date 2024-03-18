e5f61b379c122d472c07d83cff7daaf57c8d1c72
lucas <ymfs.l.j@outlook.com>
2024-03-18 16:39:18 +0800

    feat: Improve Tree component

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -1,37 +1,56 @@
 'use client';
+import { Accordion, AccordionItem } from '@nextui-org/react';
+import rightSvg from '@public/right.svg';
+import unfoldSvg from '@public/unfold.svg';
 import { TreeNode, TreeProps } from '@types';
-import React, { useState } from 'react';
+import Image from 'next/image';
+import React from 'react';
+
+const DEFAULT_ICON_SIZE = 16;
+
+const RightIcon: React.FC = () => (
+  <Image
+    src={rightSvg}
+    width={DEFAULT_ICON_SIZE}
+    height={DEFAULT_ICON_SIZE}
+    alt="Tree Right Icon"
+  />
+);
+
+const UnfoldIcon: React.FC = () => (
+  <Image
+    src={unfoldSvg}
+    width={DEFAULT_ICON_SIZE}
+    height={DEFAULT_ICON_SIZE}
+    alt="Tree Right Icon"
+  />
+);
 
 const TreeNodeComponent: React.FC<{ node: TreeNode }> = ({ node }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  // const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
+  // const toggleExpand = () => {
+  //   setIsExpanded(!isExpanded);
+  // };
 
-  return (
-    <div>
-      <div onClick={toggleExpand}>
-        {isExpanded ? '▼' : '►'} {node.name}
-      </div>
-      {isExpanded && node.children && (
-        <div style={{ marginLeft: 20 }}>
-          {node.children.map(child => (
-            <TreeNodeComponent key={child.id} node={child} />
-          ))}
-        </div>
-      )}
-    </div>
+  return node.children ? (
+    <Accordion>
+      {node.children.map(child => (
+        <TreeNodeComponent key={child.id} node={child} />
+      ))}
+    </Accordion>
+  ) : (
+    <AccordionItem>{node.name}</AccordionItem>
   );
 };
 
 const Tree: React.FC<TreeProps> = ({ data }) => {
   return (
-    <div>
+    <Accordion>
       {data.map(node => (
         <TreeNodeComponent key={node.id} node={node} />
       ))}
-    </div>
+    </Accordion>
   );
 };
 
