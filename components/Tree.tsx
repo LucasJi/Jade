a3fb13fa7b965572cf5caf6bd9036d6267c2910c
lucas <ymfs.l.j@outlook.com>
2024-03-25 22:39:51 +0800

    feat: Improve Tree component

--- components/Tree.tsx
+++ components/Tree.tsx
@@ -6,17 +6,24 @@ import { FC, useState } from 'react';
 
 const DEFAULT_ICON_SIZE = 16;
 
-const Icon: FC<{ isExpanded: boolean }> = ({ isExpanded }) => (
+const FoldIcon: FC<{ isExpanded: boolean }> = ({ isExpanded }) => (
   <svg
-    className={classNames('duration-250 ease-linear', {
+    className={classNames('transition-transform', {
       'rotate-90': isExpanded,
     })}
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1024 1024"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
     width={DEFAULT_ICON_SIZE}
     height={DEFAULT_ICON_SIZE}
+    stroke="currentColor"
   >
-    <path d="M383.291616 808.954249c-5.175883 0-10.353812-1.950422-14.338566-5.862521-8.064676-7.919367-8.182356-20.877493-0.26299-28.942169l273.602402-278.620695L368.69006 216.907145c-7.919367-8.064676-7.801686-21.022803 0.26299-28.942169 8.065699-7.918343 21.022803-7.80271 28.942169 0.26299l287.685141 292.960285c7.818059 7.961322 7.818059 20.717857 0 28.67918L397.895219 802.826692C393.887952 806.907637 388.591319 808.954249 383.291616 808.954249z" />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m8.25 4.5 7.5 7.5-7.5 7.5"
+    />
   </svg>
 );
 
@@ -33,8 +40,7 @@ const TreeNodeComponent: FC<{ node: TreeNode }> = ({ node }) => {
         onClick={toggleExpand}
         className="flex items-center border-1 border-green-700"
       >
-        {/* {isExpanded ? <Icon isExpanded /> : <FoldIcon />} */}
-        <Icon isExpanded={isExpanded} />
+        <FoldIcon isExpanded={isExpanded} />
         <span className="font-semibold max-w-[200px] text-base inline-block truncate">
           {node.name}
         </span>
@@ -43,13 +49,13 @@ const TreeNodeComponent: FC<{ node: TreeNode }> = ({ node }) => {
         className={classNames(
           'border-1 border-orange-700',
           isExpanded ? 'h-[100px]' : 'h-0',
-          'transition-all',
+          'transition-height',
         )}
       >
         <ul
           className={classNames(
             'ml-4',
-            'transition-all',
+            'transition-opacity',
             isExpanded ? 'opacity-100' : 'opacity-0',
             isExpanded ? 'visible' : 'invisible',
           )}
