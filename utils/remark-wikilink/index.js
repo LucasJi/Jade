cf58e102220331061e7e995abef50ce54edda385
lucas <ymfs.l.j@outlook.com>
2024-03-26 18:36:22 +0800

    chore: Format somve file

--- utils/remark-wikilink/index.js
+++ utils/remark-wikilink/index.js
@@ -3,23 +3,23 @@ import syntax from './syntax';
 
 // import toMarkdown from './toMarkdown';
 
-function remarkWikilink(opts={markdownFolder: 'page'}) {
-  const data=this.data();
+function remarkWikilink(opts = { markdownFolder: 'page' }) {
+  const data = this.data();
 
-  function add(field,value) {
-    if(data[field]) {
+  function add(field, value) {
+    if (data[field]) {
       data[field].push(value);
     } else {
-      data[field]=[value];
+      data[field] = [value];
     }
   }
 
-  opts={
+  opts = {
     ...opts,
   };
 
-  add('micromarkExtensions',syntax(opts));
-  add('fromMarkdownExtensions',fromMarkdown(opts));
+  add('micromarkExtensions', syntax(opts));
+  add('fromMarkdownExtensions', fromMarkdown(opts));
   // TODO: toMarkdown extension seems doesn't work
   // add('toMarkdownExtensions', toMarkdown(opts));
 }
@@ -28,6 +28,5 @@ export {
   fromMarkdown as fromMarkdownWikilink,
   remarkWikilink,
   // toMarkdown,
-  syntax
+  syntax,
 };
-
