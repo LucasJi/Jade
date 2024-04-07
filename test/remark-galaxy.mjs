6ff014280d4d479bd60bff8ab0843fb0236ff5be
lucas <ymfs.l.j@outlook.com>
2024-04-07 17:29:59 +0800

    bugfix: remark-wikilink doesn't work

--- test/remark-galaxy.mjs
+++ test/remark-galaxy.mjs
@@ -1,8 +1,6 @@
-import rehypeStringify from 'rehype-stringify';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkParse from 'remark-parse';
 import remarkParseFrontmatter from 'remark-parse-frontmatter';
-import remarkRehype from 'remark-rehype';
 import remarkStringify from 'remark-stringify';
 import { read } from 'to-vfile';
 import { unified } from 'unified';
@@ -13,19 +11,19 @@ const filepath1 = '_posts/tech/java/jdk21.md';
 const filepath2 = '/Users/lucas/Projects/docs/index.md';
 
 const processor = unified()
-  .use(remarkParse)
   .use(remarkWikilink)
+  .use(remarkParse)
   .use(remarkFrontmatter, ['yaml'])
   .use(remarkParseFrontmatter)
   .use(remarkGalaxy, { title: 'Test Title' })
-  .use(remarkStringify)
-  .use(remarkRehype)
-  .use(rehypeStringify);
+  .use(remarkStringify);
+// .use(remarkRehype)
+// .use(rehypeStringify);
 
 const post = await read(filepath1);
 
 const file = processor.processSync(post);
 // const root = processor.parse(post);
 
-// console.log(file);
+console.log(file);
 // console.log('root', root);
