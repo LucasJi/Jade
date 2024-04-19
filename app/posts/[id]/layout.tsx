83c053ed742135928b3b697ade6fcb39d971e4cc
lucas <ymfs.l.j@outlook.com>
2024-04-19 17:06:22 +0800

    style: Adjust post page layout

--- app/posts/[id]/layout.tsx
+++ app/posts/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import GraphView from '@components/GraphView';
 import Toc from '@components/Toc';
+import Tree from '@components/Tree';
 import { PostGraph } from '@types';
 import { getIds } from '@utils/postUtil';
 import { ReactNode } from 'react';
@@ -22,10 +23,20 @@ export default async function Layout({
     },
   ).then(resp => resp.json());
 
+  const postTree = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/tree`,
+    {
+      method: 'GET',
+    },
+  ).then(resp => resp.json());
+
   return (
     <div className="flex w-full h-full">
-      <div className="w-2/3 p-4 flex-1 overflow-y-auto">{children}</div>
-      <div className="w-1/3 p-4 flex flex-col overflow-y-auto">
+      <div className="w-1/4 p-4 flex justify-end">
+        <Tree data={postTree} className="w-[300px]" />
+      </div>
+      <div className="w-1/2 p-4 flex-1 overflow-y-auto">{children}</div>
+      <div className="w-1/4 p-4 flex flex-col overflow-y-auto">
         <GraphView postGraph={postGraph} postId={decodedId} />
         <Toc id={id} className="mt-4" />
       </div>
--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,20 +1,5 @@
-import Tree from '@components/Tree';
 import { ReactNode } from 'react';
 
 export default async function Layout({ children }: { children: ReactNode }) {
-  const postTree = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/tree`,
-    {
-      method: 'GET',
-    },
-  ).then(resp => resp.json());
-
-  return (
-    <div className="flex w-full h-full py-10">
-      <div className="w-1/4 p-4 flex justify-end">
-        <Tree data={postTree} className="w-[300px]" />
-      </div>
-      <div className="w-3/4">{children}</div>
-    </div>
-  );
+  return <div className="flex w-full h-full py-10">{children}</div>;
 }
