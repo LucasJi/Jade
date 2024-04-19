fdeb0abf51476d9df9df258d1379e7717a26358f
lucas <ymfs.l.j@outlook.com>
2024-04-19 17:06:59 +0800

    style: Adjust post page layout

---
+++ app/posts/page.tsx
@@ -0,0 +1,17 @@
+import GraphView from '@components/GraphView';
+import { PostGraph } from '@types';
+
+export default async function Page() {
+  const postGraph: PostGraph = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/graph`,
+    {
+      method: 'GET',
+    },
+  ).then(resp => resp.json());
+
+  return (
+    <div className="flex w-full justify-center">
+      <GraphView postGraph={postGraph} className="mt-8" />
+    </div>
+  );
+}
