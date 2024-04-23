cb837ea60dcd88b0defeba503248425d0b9f6b93
lucas <ymfs.l.j@outlook.com>
2024-04-23 16:56:42 +0800

    Remove next-mdx related code

--- app/mdx/page.tsx
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-
-export default async function Page() {
-  const filepath = '_posts/Aliases/Add an alias to a note.md';
-  const post = readFileSync(filepath);
-
-  return <MDXRemote source={post} />;
-}
--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-import createMDX from '@next/mdx';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
   swcMinify: true,
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
