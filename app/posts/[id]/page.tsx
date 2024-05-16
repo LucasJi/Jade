cd4d8c4eff9b60999bdb35e993e1a12c3fff89a5
lucas <ymfs.l.j@outlook.com>
2024-05-16 15:12:26 +0800

    feat: Update post not found page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -5,12 +5,12 @@ import { getPostIds } from '@/utils/getPostIds';
 import GraphView from '@components/GraphView';
 import Markdown from '@components/Markdown';
 import Toc from '@components/Toc';
+import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
   try {
     return getPostIds();
   } catch (error) {
-    console.error('Error generating static params:', error);
     return [];
   }
 }
@@ -20,24 +20,28 @@ export default async function Page({
 }: {
   params: { id: string };
 }) {
-  const decodedId = decodeURIComponent(id);
-  const post = await getPostById(decodedId);
-  const adjPosts = await getAdjacencyPostsById(post!.id);
-  const postGraph = await getPostGraphFromPosts(adjPosts);
+  try {
+    const decodedId = decodeURIComponent(id);
+    const post = await getPostById(decodedId);
+    const adjPosts = await getAdjacencyPostsById(post!.id);
+    const postGraph = await getPostGraphFromPosts(adjPosts);
 
-  if (!post) {
-    return <div>Post not found</div>;
+    if (!post) {
+      return <div>Post not found</div>;
+    }
+
+    return (
+      <div className="flex w-full h-full">
+        <div className="w-2/3 px-4 flex-1 overflow-y-auto min-h-[600px]">
+          <Markdown post={post} className="max-w-none w-full" />
+        </div>
+        <div className="w-1/3 px-4 flex flex-col overflow-y-auto">
+          <GraphView postGraph={postGraph} postId={decodedId} />
+          <Toc content={post?.content} className="mt-4" />
+        </div>
+      </div>
+    );
+  } catch (error) {
+    notFound();
   }
-
-  return (
-    <div className="flex w-full h-full">
-      <div className="w-2/3 px-4 flex-1 overflow-y-auto min-h-[600px]">
-        <Markdown post={post} className="max-w-none w-full" />
-      </div>
-      <div className="w-1/3 px-4 flex flex-col overflow-y-auto">
-        <GraphView postGraph={postGraph} postId={decodedId} />
-        <Toc content={post?.content} className="mt-4" />
-      </div>
-    </div>
-  );
 }
