29b2258c4461aa03b13baa0bfefc526295813df7
lucas <ymfs.l.j@outlook.com>
2024-07-23 16:40:18 +0800

    chore: try to use pixi.js to improve graph view

--- components/pixi-demo.jsx
+++ components/pixi-demo.jsx
@@ -28,20 +28,88 @@ export default function PixiDemo({ postGraph }) {
       return () => {};
     }
 
-    const app = new Application();
-
     const radius = 5,
       lines = new Graphics(),
       circles = new Container(),
-      baseColor = '#838383',
+      baseColor = '#5c5c5c',
       hlColor = '#a88bfa',
-      hlTintColor = 'rgba(168,139,250,0)',
-      noHlColor = 'rgba(156,163,175,1)';
+      noHlColor = '#9ca3af80',
+      links = postGraph.links,
+      nodes = postGraph.nodes,
+      app = new Application(),
+      duration = 2000, // Duration in milliseconds
+      simulation = forceSimulation()
+        .force('charge', forceManyBody())
+        .force('x', forceX(width / 2))
+        .force('y', forceY(height / 2));
 
-    let transform = zoomIdentity.translate(width / 2, height / 2);
+    let transform = zoomIdentity.translate(width / 2, height / 2),
+      elapsed = 0;
 
-    const links = postGraph.links;
-    const nodes = postGraph.nodes;
+    const dragSubject = event => {
+      return simulation.find(
+        transform.invertX(event.x),
+        transform.invertY(event.y),
+        radius,
+      );
+    };
+
+    const dragStarted = event => {
+      if (!event.active) {
+        simulation.alphaTarget(0.3).restart();
+      }
+
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    };
+
+    const dragged = event => {
+      event.subject.fx += event.dx / transform.k;
+      event.subject.fy += event.dy / transform.k;
+
+      draggingRef.current = true;
+      overedNodeRef.current = event.subject.id;
+    };
+
+    const dragEnded = event => {
+      if (!event.active) {
+        simulation.alphaTarget(0);
+      }
+
+      event.subject.fx = null;
+      event.subject.fy = null;
+
+      event.subject.tint = null;
+      draggingRef.current = false;
+      overedNodeRef.current = null;
+    };
+
+    const zoomed = event => {
+      transform = event.transform;
+
+      app.stage.x = transform.x;
+      app.stage.y = transform.y;
+      app.stage.scale.x = transform.k;
+      app.stage.scale.y = transform.k;
+    };
+
+    const drawLines = () => {
+      lines.clear();
+
+      for (const link of links) {
+        lines.moveTo(link.source.x, link.source.y);
+        lines.lineTo(link.target.x, link.target.y);
+        if (overedNodeRef.current) {
+          if (link.source.id === overedNodeRef.current) {
+            lines.stroke({ width: 0.2, color: hlColor, alpha: 1 });
+          } else {
+            lines.stroke({ width: 0.2, color: baseColor, alpha: 0.1 });
+          }
+        } else {
+          lines.stroke({ width: 0.2, color: baseColor, alpha: 1 });
+        }
+      }
+    };
 
     app
       .init({
@@ -56,139 +124,58 @@ export default function PixiDemo({ postGraph }) {
       .then(() => {
         document.getElementById('graph-view').appendChild(app.canvas);
 
-        const simulation = forceSimulation()
-          .force('charge', forceManyBody())
-          .force('x', forceX(width / 2))
-          .force('y', forceY(height / 2));
-
-        const dragSubject = event => {
-          return simulation.find(
-            transform.invertX(event.x),
-            transform.invertY(event.y),
-            radius,
-          );
-        };
-
-        const dragStarted = event => {
-          if (!event.active) {
-            simulation.alphaTarget(0.3).restart();
-          }
-
-          event.subject.fx = event.subject.x;
-          event.subject.fy = event.subject.y;
-        };
-
-        const dragged = event => {
-          event.subject.fx += event.dx / transform.k;
-          event.subject.fy += event.dy / transform.k;
-
-          event.subject.tint = hlTintColor;
-          draggingRef.current = true;
-          overedNodeRef.current = event.subject.id;
-        };
-
-        const dragEnded = event => {
-          if (!event.active) {
-            simulation.alphaTarget(0);
-          }
-
-          event.subject.fx = null;
-          event.subject.fy = null;
-
-          event.subject.tint = null;
-          draggingRef.current = false;
-          overedNodeRef.current = null;
-        };
-
         app.stage.addChild(lines);
         app.stage.addChild(circles);
 
-        for (let i = 0; i < nodes.length; i++) {
-          const node = nodes[i];
-          nodeIdxMapRef.current.set(node.id, i);
+        const drawCircles = color => {
+          circles.removeChildren();
+          for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            nodeIdxMapRef.current.set(node.id, i);
 
-          const circle = new Graphics()
-            .circle(0, 0, radius)
-            .fill({ color: baseColor, alpha: 1 });
-          circle.id = node.id;
+            const circle = new Graphics()
+              .circle(node.x, node.y, radius)
+              .fill({ color, alpha: 1 });
+            circle.id = node.id;
 
-          // accept events, trigger hover status
-          circle.eventMode = 'static';
-          circle.cursor = 'pointer';
+            // accept events, trigger hover status
+            circle.eventMode = 'static';
+            circle.cursor = 'pointer';
 
-          // events
-          circle
-            .on('pointerover', function () {
-              if (!draggingRef.current) {
-                this.tint = hlTintColor;
-                overedNodeRef.current = this.id;
-                redraw();
-                const overedNodeIdx = nodeIdxMapRef.current.get(this.id);
-                const overedNode = nodes[overedNodeIdx];
-                circles.children.forEach(child => {
-                  if (
-                    child.id !== this.id &&
-                    !overedNode.forwardLinks.includes(child.id)
-                  ) {
-                    child.tint = noHlColor;
-                    child.alpha = 0.1;
-                  }
-                });
-              }
-            })
-            .on('pointerout', function () {
-              if (!draggingRef.current) {
-                overedNodeRef.current = null;
-                redraw();
-                circles.children.forEach(child => {
-                  child.tint = null;
-                  child.alpha = 1;
-                });
-              }
-            });
+            // events
+            circle
+              .on('pointerover', function () {
+                if (!draggingRef.current) {
+                  overedNodeRef.current = this.id;
+                  // drawLines();
+                  this.clear();
+                  this.circle(node.x, node.y, radius).fill(hlColor);
+                  app.renderer.render(app.stage);
+                }
+              })
+              .on('pointerout', function () {
+                if (!draggingRef.current) {
+                  overedNodeRef.current = null;
+                  drawLines();
+                  this.clear();
+                  this.circle(node.x, node.y, radius).fill(baseColor);
+                  app.renderer.render(app.stage);
+                  elapsed = 0;
+                }
+              });
 
-          circles.addChild(circle);
-        }
-
-        const zoomed = event => {
-          transform = event.transform;
-
-          app.stage.x = transform.x;
-          app.stage.y = transform.y;
-          app.stage.scale.x = transform.k;
-          app.stage.scale.y = transform.k;
-        };
-
-        const redraw = () => {
-          lines.clear();
-
-          for (const link of links) {
-            lines.moveTo(link.source.x, link.source.y);
-            lines.lineTo(link.target.x, link.target.y);
-            if (overedNodeRef.current) {
-              if (link.source.id === overedNodeRef.current) {
-                lines.stroke({ width: 0.2, color: hlColor, alpha: 1 });
-              } else {
-                lines.stroke({ width: 0.2, color: baseColor, alpha: 0.1 });
-              }
-            } else {
-              lines.stroke({ width: 0.2, color: baseColor, alpha: 1 });
-            }
+            circles.addChild(circle);
           }
         };
+        drawCircles(baseColor);
 
         simulation
-          .on('tick', redraw)
+          .on('tick', drawLines)
           .nodes(circles.children)
           .force(
             'link',
             forceLink(links).id(d => d.id),
           );
-
-        // app.ticker.add(function updateGraphLinks(delta) {
-        //   simulation.tick();
-        //   redraw();
-        // });
 
         select(app.canvas)
           .call(
@@ -203,6 +190,46 @@ export default function PixiDemo({ postGraph }) {
               .scaleExtent([1 / 10, 8])
               .on('zoom', zoomed),
           );
+
+        // change color gradually
+        app.ticker.add(delta => {
+          if (!overedNodeRef.current) {
+            return;
+          }
+
+          elapsed += delta.elapsedMS;
+          const factor = Math.min(elapsed / duration, 1);
+
+          if (factor >= 1) {
+            return;
+          }
+
+          const overedNodeIdx = nodeIdxMapRef.current.get(
+            overedNodeRef.current,
+          );
+          const overedNode = nodes[overedNodeIdx];
+
+          lines.clear();
+          for (const link of links) {
+            lines.moveTo(link.source.x, link.source.y);
+            lines.lineTo(link.target.x, link.target.y);
+            if (overedNodeRef.current) {
+              if (link.source.id === overedNodeRef.current) {
+                lines.stroke({ width: 0.2, color: hlColor, alpha: 1 });
+              } else {
+                lines.stroke({
+                  width: 0.2,
+                  color: baseColor,
+                  alpha: 1 - 0.9 * factor,
+                });
+              }
+            } else {
+              lines.stroke({ width: 0.2, color: baseColor, alpha: 1 });
+            }
+          }
+        });
+
+        // recover color gradually
       });
 
     mountedRef.current = true;
