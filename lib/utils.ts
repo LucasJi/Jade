29453011fae50734aaf1f313d800dce1ae31b691
lucas <lucasji@foxmail.com>
2024-10-16 17:02:55 +0800

    refactor: rename several libs

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,19 @@ export function cn(...inputs: ClassValue[]) {
 export function getFileExt(filename: string): string {
   return filename.slice(((filename.lastIndexOf('.') - 1) >>> 0) + 2);
 }
+
+export class GlobalRef<T> {
+  private readonly sym: symbol;
+
+  constructor(uniqueName: string) {
+    this.sym = Symbol.for(uniqueName);
+  }
+
+  get value() {
+    return (global as any)[this.sym] as T;
+  }
+
+  set value(value: T) {
+    (global as any)[this.sym] = value;
+  }
+}
