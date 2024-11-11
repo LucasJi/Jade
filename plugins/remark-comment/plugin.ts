315581613ca3c5b92eb4313cbdd7bbb2b6b78905
lucas <lucasji@foxmail.com>
2024-11-11 16:32:36 +0800

    chore: remove useless plugins

--- plugins/remark-comment/plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Options } from '@/plugins/remark-callout';
-import type { Root } from 'mdast';
-import type { Plugin } from 'unified';
-
-export const remarkComment: Plugin<[Options?], Root> = () => {
-  return (tree, _) => {};
-};
