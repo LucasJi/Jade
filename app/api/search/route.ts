2f0ecf5c0b796d1929e4650ce6313c02ee42f6a7
lucas <lucasji@foxmail.com>
2024-12-31 15:55:30 +0800

    feat: improve search algorithm

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,13 @@
+import { logger } from '@/lib/logger';
 import { createRedisClient } from '@/lib/redis';
 import { trim } from 'lodash';
 import { NextRequest, NextResponse } from 'next/server';
+import { RedisSearchLanguages } from 'redis';
+
+const log = logger.child({
+  module: 'api',
+  route: 'api/search',
+});
 
 const redis = await createRedisClient();
 
@@ -9,8 +16,21 @@ export async function GET(req: NextRequest) {
   let content = searchParams.get('content') || '';
   content = trim(content)
     .split(' ')
-    .map(c => `${c}*`)
-    .join(' ');
-  const result = await redis.ft.search('jade:idx:hChld', `${content}`);
-  return NextResponse.json(result);
+    .map(c => `${c}|*${c}*|%${c}%`)
+    .join('|');
+
+  log.info({ content }, 'Searching with content...');
+
+  try {
+    const result = await redis.ft.search('jade:idx:hChld', `${content}`, {
+      LANGUAGE: RedisSearchLanguages.CHINESE,
+    });
+    return NextResponse.json(result);
+  } catch (e) {
+    log.error('Error occurs when searching...', e);
+    return NextResponse.json({
+      total: 0,
+      documents: [],
+    });
+  }
 }
