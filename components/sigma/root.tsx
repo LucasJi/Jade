be0e0502122c572879cc5d801770194788d267fe
lucas <lucasji@foxmail.com>
2025-01-09 13:17:42 +0800

    feat: update Sigma component

--- components/sigma/root.tsx
+++ components/sigma/root.tsx
@@ -1,31 +1,15 @@
 'use client';
 
-import {
-  FullScreenControl,
-  SigmaContainer,
-  ZoomControl,
-} from '@react-sigma/core';
+import { SigmaContainer } from '@react-sigma/core';
 import { createNodeImageProgram } from '@sigma/node-image';
 import { DirectedGraph } from 'graphology';
 import { constant, keyBy, mapValues, omit } from 'lodash';
 import { FC, useEffect, useMemo, useState } from 'react';
-import { BiBookContent, BiRadioCircleMarked } from 'react-icons/bi';
-import {
-  BsArrowsFullscreen,
-  BsFullscreenExit,
-  BsZoomIn,
-  BsZoomOut,
-} from 'react-icons/bs';
-import { GrClose } from 'react-icons/gr';
 import { Settings } from 'sigma/settings';
 
 import { drawHover, drawLabel } from './canvas-utils';
 import ClustersPanel from './clusters-panel';
-import DescriptionPanel from './description-panel';
 import GraphDataController from './graph-data-controller';
-import GraphEventsController from './graph-events-controller';
-import GraphSettingsController from './graph-settings-controller';
-import GraphTitle from './graph-title';
 import SearchField from './search-field';
 import './style.css';
 import TagsPanel from './tags-panel';
@@ -33,14 +17,12 @@ import { Dataset, FiltersState } from './types';
 
 const Root: FC = () => {
   const graph = useMemo(() => new DirectedGraph(), []);
-  const [showContents, setShowContents] = useState(false);
   const [dataReady, setDataReady] = useState(false);
   const [dataset, setDataset] = useState<Dataset | null>(null);
   const [filtersState, setFiltersState] = useState<FiltersState>({
     clusters: {},
     tags: {},
   });
-  const [hoveredNode, setHoveredNode] = useState<string | null>(null);
   const sigmaSettings: Partial<Settings> = useMemo(
     () => ({
       nodeProgramClasses: {
@@ -113,55 +95,34 @@ const Root: FC = () => {
   }
 
   return (
-    <div id="app-root" className={showContents ? 'show-contents' : ''}>
+    <div id="app-root" className={'show-contents'}>
       <SigmaContainer
         graph={DirectedGraph}
         settings={sigmaSettings}
         className="react-sigma"
       >
-        <GraphSettingsController hoveredNode={hoveredNode} />
-        <GraphEventsController setHoveredNode={setHoveredNode} />
+        {/*<GraphSettingsController hoveredNode={hoveredNode} />*/}
+        {/*<GraphEventsController setHoveredNode={setHoveredNode} />*/}
         <GraphDataController dataset={dataset} filters={filtersState} />
 
         {dataReady && (
           <>
             <div className="controls">
-              <div className="react-sigma-control ico">
-                <button
-                  type="button"
-                  className="show-contents"
-                  onClick={() => setShowContents(true)}
-                  title="Show caption and description"
-                >
-                  <BiBookContent />
-                </button>
-              </div>
-              <FullScreenControl className="ico">
-                <BsArrowsFullscreen />
-                <BsFullscreenExit />
-              </FullScreenControl>
+              {/*<FullScreenControl className="ico">*/}
+              {/*  <BsArrowsFullscreen />*/}
+              {/*  <BsFullscreenExit />*/}
+              {/*</FullScreenControl>*/}
 
-              <ZoomControl className="ico">
-                <BsZoomIn />
-                <BsZoomOut />
-                <BiRadioCircleMarked />
-              </ZoomControl>
+              {/*<ZoomControl className="ico">*/}
+              {/*  <BsZoomIn />*/}
+              {/*  <BsZoomOut />*/}
+              {/*  <BiRadioCircleMarked />*/}
+              {/*</ZoomControl>*/}
             </div>
             <div className="contents">
-              <div className="ico">
-                <button
-                  type="button"
-                  className="ico hide-contents"
-                  onClick={() => setShowContents(false)}
-                  title="Show caption and description"
-                >
-                  <GrClose />
-                </button>
-              </div>
-              <GraphTitle filters={filtersState} />
+              {/*<GraphTitle filters={filtersState} />*/}
               <div className="panels">
                 <SearchField filters={filtersState} />
-                <DescriptionPanel />
                 <ClustersPanel
                   clusters={dataset.clusters}
                   filters={filtersState}
--- components/sigma/use-debounce.ts
+++ components/sigma/use-debounce.ts
@@ -16,8 +16,8 @@ function useDebounce<T>(value: T, delay: number): T {
       }, delay);
 
       // Cancel the timeout if value changes (also on delay change or unmount)
-      // This is how we prevent debounced value from updating if value is changed ...
-      // .. within the delay period. Timeout gets cleared and restarted.
+      // This is how we prevent debounced value from updating if value is changed
+      // within the delay period. Timeout gets cleared and restarted.
       return () => {
         clearTimeout(handler);
       };
