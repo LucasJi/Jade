cd93c8db9ef9aad61f2c55123767e377150000bd
lucas <lucasji@foxmail.com>
2025-01-15 09:47:04 +0800

    test: test aws-s3-client with minio

--- lib/__test__/s3.test.ts
+++ lib/__test__/s3.test.ts
@@ -1,31 +1,35 @@
 import {
   BucketLocationConstraint,
+  BucketVersioningStatus,
   CreateBucketCommand,
   CreateBucketCommandInput,
   ListBucketsCommand,
   ListObjectsV2Command,
   ListObjectsV2CommandInput,
+  ListObjectVersionsCommand,
+  ListObjectVersionsCommandInput,
+  PutBucketVersioningCommand,
+  PutBucketVersioningCommandInput,
+  PutObjectCommand,
+  PutObjectCommandInput,
   S3Client,
 } from '@aws-sdk/client-s3';
 import { describe, test } from 'vitest';
 
-const bucket = 'jade-docs';
+const bucket = 'test';
 
 describe('s3 apis', () => {
-  const s3client = new S3Client({
-    // specify endpoint with http://hostname:port
-    endpoint: `http://127.0.0.1:8333`,
-    // specify region since it is mandatory, but it will be ignored by seaweedfs
+  const client = new S3Client({
+    endpoint: 'http://127.0.0.1:9000',
     region: BucketLocationConstraint.cn_north_1,
-    // force path style for compatibility reasons
     forcePathStyle: true,
-    // credentials is mandatory and s3 authorization should be enabled with `s3.configure`
     credentials: {
-      accessKeyId: `123456`,
-      secretAccessKey: `abcdef`,
+      accessKeyId: 'BfXrvWd59Q6rXwT8Ev5f',
+      secretAccessKey: 'KNC6jkig4fAH0GEu0G3FJLdImBNhBNNMhIB8oqtt',
     },
   });
-  test('createBucket', async () => {
+
+  test('CreateBucket', async () => {
     const input: CreateBucketCommandInput = {
       Bucket: 'test',
       CreateBucketConfiguration: {
@@ -34,22 +38,55 @@ describe('s3 apis', () => {
     };
 
     const command = new CreateBucketCommand(input);
-    const response = await s3client.send(command);
+    const response = await client.send(command);
+    console.log(response);
+  });
+
+  test('EnableBucketVersioning', async () => {
+    const input: PutBucketVersioningCommandInput = {
+      Bucket: bucket,
+      VersioningConfiguration: {
+        Status: BucketVersioningStatus.Enabled,
+      },
+    };
+    const command = new PutBucketVersioningCommand(input);
+    const response = await client.send(command);
     console.log(response);
   });
 
   test('ListBuckets', async () => {
-    const response = await s3client.send(new ListBucketsCommand({}));
+    const response = await client.send(new ListBucketsCommand({}));
     console.log(response.Buckets);
+  });
+
+  test('PutObject', async () => {
+    const input: PutObjectCommandInput = {
+      Body: 'filetoupload_1',
+      Bucket: bucket,
+      Key: 's3UploadTestFile_1.txt',
+    };
+    const command = new PutObjectCommand(input);
+    const response = await client.send(command);
+    console.log(response);
+  });
+
+  test('ListObjectVersions', async () => {
+    const input: ListObjectVersionsCommandInput = {
+      Bucket: bucket,
+      Prefix: '',
+    };
+    const command = new ListObjectVersionsCommand(input);
+    const response = await client.send(command);
+    console.log(response);
   });
 
   test('ListObjectsV2', async () => {
     const input: ListObjectsV2CommandInput = {
-      Bucket: 'test',
+      Bucket: bucket,
       MaxKeys: 1000,
     };
     const command = new ListObjectsV2Command(input);
-    const response = await s3client.send(command);
+    const response = await client.send(command);
     console.log(response);
   });
 });
