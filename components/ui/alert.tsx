f01bfb6a8879fa09dd1e315c9f407547f490274b
lucas <lucasji@foxmail.com>
2025-01-16 16:06:02 +0800

    chore: remove some comments

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -3,39 +3,6 @@ import * as React from 'react';
 
 import { cn } from '@/components/utils';
 
-// const generateVariant = (variant: string) =>
-//   `border-${variant}/50 text-${variant} dark:border-${variant} [&>svg]:text-${variant}`;
-//
-// enum Variant {
-//   destructive,
-//   bug,
-//   error,
-//   example,
-//   fail,
-//   important,
-//   info,
-//   question,
-//   success,
-//   summary,
-//   tip,
-//   todo,
-//   warning,
-//   quote,
-// }
-
-// type VariantType = keyof typeof Variant;
-
-// const generateVariants = () => {
-//   const variants: Partial<Record<VariantType, any>> = {};
-//   Object.keys(Variant)
-//     .filter(v => isNaN(Number(v)))
-//     .forEach(k => {
-//       variants[k as VariantType] = generateVariant(k);
-//     });
-//
-//   return variants;
-// };
-
 const alertVariants = cva(
   'relative w-full rounded-lg border p-4 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7',
   {
