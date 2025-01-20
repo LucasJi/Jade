47b95ef10950cee72c8af8cbb03408b5d8b3ce37
lucas <lucasji@foxmail.com>
2025-01-20 10:28:37 +0800

    feat: revalidate sitemap every 30 seconds

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,6 +4,9 @@ import type { MetadataRoute } from 'next';
 
 const s3 = new S3();
 
+export const revalidate = 30;
+export const dynamic = 'force-static';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const objs = listExistedObjs(await s3.listObjectVersions());
   const result = [
