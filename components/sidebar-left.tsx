ae534a255aa4d98e915984f9e680c124e19dbbbd
lucas <lucasji@foxmail.com>
2025-02-13 13:28:13 +0800

    feat: open selected note's parent folders by default

--- components/sidebar-left.tsx
+++ components/sidebar-left.tsx
@@ -34,6 +34,7 @@ import { useRouter } from 'next/navigation';
 import {
   ComponentProps,
   createContext,
+  useCallback,
   useContext,
   useEffect,
   useState,
@@ -41,28 +42,49 @@ import {
 
 const TreeViewContext = createContext<{
   noteRoutePath: string;
-  folders: string[];
+  folders: Set<string>;
+  openFolders: (opened: string[]) => void;
+  closeFolder: (closed: string) => void;
 }>({
   noteRoutePath: '',
-  folders: [],
+  folders: new Set(),
+  openFolders: () => {},
+  closeFolder: () => {},
 });
 
 export function SidebarLeft({ ...props }: ComponentProps<typeof Sidebar>) {
   const [treeNodes, setTreeNodes] = useState<TreeViewNode[]>([]);
-  const { parsingRoute, routePath, vaultPath } = useSidebar();
-  const [_, ...folders] = vaultPath.split('/').reverse();
+  const { routePath, vaultPath } = useSidebar();
+  const [folders, setFolders] = useState<Set<string>>(new Set());
+
+  const openFolders = useCallback((opened: string[]) => {
+    setFolders(pre => {
+      opened.forEach(f => pre.add(f));
+      return new Set(pre);
+    });
+  }, []);
+
+  const closeFolder = useCallback((closed: string) => {
+    setFolders(pre => {
+      pre.delete(closed);
+      return new Set(pre);
+    });
+  }, []);
 
   useEffect(() => {
-    if (!parsingRoute) {
-      getTreeView().then(data => {
-        setTreeNodes(data);
-      });
-    }
-  }, [parsingRoute]);
+    getTreeView().then(data => {
+      setTreeNodes(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const [_, ...folders] = vaultPath.split('/').reverse();
+    openFolders(folders);
+    console.log(vaultPath);
+  }, [vaultPath, openFolders]);
 
   return (
     <Sidebar {...props}>
-      {/*<SidebarHeader>Omni Note</SidebarHeader>*/}
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Files</SidebarGroupLabel>
@@ -72,6 +94,8 @@ export function SidebarLeft({ ...props }: ComponentProps<typeof Sidebar>) {
                 value={{
                   noteRoutePath: routePath,
                   folders,
+                  openFolders,
+                  closeFolder,
                 }}
               >
                 {treeNodes.map((item, index) => (
@@ -89,7 +113,8 @@ export function SidebarLeft({ ...props }: ComponentProps<typeof Sidebar>) {
 
 function Tree({ item }: { item: TreeViewNode }) {
   const router = useRouter();
-  const { folders, noteRoutePath } = useContext(TreeViewContext);
+  const { folders, noteRoutePath, openFolders, closeFolder } =
+    useContext(TreeViewContext);
   if (!item.isDir) {
     let Icon;
     if (isMd(item.path)) {
@@ -109,11 +134,11 @@ function Tree({ item }: { item: TreeViewNode }) {
     return (
       <SidebarMenuButton
         isActive={item.path === noteRoutePath}
-        className="data-[active=true]:bg-transparent data-[active=true]:text-black"
+        className="data-[active=true]:text-black"
         onClick={() => router.push(`/notes/${item.path || ''}`)}
       >
         <Icon />
-        <span className="truncate" title={item.path}>
+        <span className="truncate" title={item.name}>
           {item.name}
         </span>
       </SidebarMenuButton>
@@ -124,13 +149,20 @@ function Tree({ item }: { item: TreeViewNode }) {
     <SidebarMenuItem>
       <Collapsible
         className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
-        defaultOpen={folders.includes(item.name)}
+        open={folders.has(item.name)}
+        onOpenChange={open => {
+          if (open) {
+            openFolders([item.name]);
+          } else {
+            closeFolder(item.name);
+          }
+        }}
       >
         <CollapsibleTrigger asChild>
           <SidebarMenuButton>
             <ChevronRight className="transition-transform" />
             <Folder />
-            <span className="truncate" title={item.path}>
+            <span className="truncate" title={item.name}>
               {item.name}
             </span>
           </SidebarMenuButton>
