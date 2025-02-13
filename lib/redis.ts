ed04ffa6b16d2933e65a7b3c322561bd351baf7a
lucas <lucasji@foxmail.com>
2025-02-13 15:04:07 +0800

    build: update docker compose file and remove useless configs

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,12 +1,14 @@
 import { logger } from '@/lib/logger';
 import { createClient } from 'redis';
-import config from './config';
 
 const log = logger.child({ module: 'lib:redis' });
 
 export const createRedisClient = async () => {
+  const host = process.env.REDIS_HOST || '127.0.0.1';
+  const port = Number.parseInt(process.env.REDIS_PORT || '') || 6379;
+  const pass = process.env.REDIS_PASS;
   return await createClient({
-    url: `redis://:${config.redis.pass}@${config.redis.host}:${config.redis.port}`,
+    url: `redis://:${pass}@${host}:${port}`,
   })
     .on('error', err => log.error({ error: err }, 'Redis Client Error'))
     .on('ready', () => {})
